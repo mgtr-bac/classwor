b35d028abb97be365bff7cd80791d3d885e4d201
mgtr-bac <bmarys.2009@gmail.com>
2026-02-16 18:11:12 +0300

    2

--- app.py
+++ app.py
@@ -7,11 +7,20 @@ app = Flask(__name__)
 def main():
     return 'Миссия Колонизация Марса'
 
-
 @app.route('/index')
 def new():
     return 'И на Марсе будут яблони цвести!'
 
+@app.route('/promotion')
+def promotion():
+    promo_text = (
+        "Человечество вырастает из детства.<br>"
+        "Человечеству мала одна планета.<br>"
+        "Мы сделаем обитаемыми безжизненные пока планеты.<br>"
+        "И начнем с Марса!<br>"
+        "Присоединяйся!<br>"
+    )
+    return promo_text
 
 if __name__ == "__main__":
     app.run("127.0.0.1", 8080)
